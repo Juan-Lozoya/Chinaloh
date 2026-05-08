7fbf29f856dff4f576d6996cba758d7c5613bda6
Fabio Cuevas <fcuevas2505@gmail.com>
2026-05-08 10:38:31 -0600

    Correcciones finales: ui buscador y docker

--- frontend/src/routes/dashboard/home.tsx
+++ frontend/src/routes/dashboard/home.tsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import { Accordion } from "@heroui/react";
 import { searchMovie } from "@/services/search.service";
 
+interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+}
+
 export default function HomeDashboard() {
   const [search, setSearch] = useState<string>("");
   const [data, setData] = useState([]);
@@ -23,22 +30,22 @@ export default function HomeDashboard() {
         <Input
           value={search}
           type="text"
-          placeholder="Search for a movie..."
+          placeholder="Busca tu pelicula..."
           onChange={(e) => setSearch(e.target.value)}
           className="w-80"
         />
         <Button onClick={handleSearch} className="bg-black text-white">
-          Search
+          Buscar
         </Button>
       </div>
 
       {/* Resultados si existe data */}
-      {data.length > 0 && (
+      {data.length ? (
         <div className="flex flex-col md:flex-row w-full max-w-7xl mx-auto gap-8 transition-all duration-500 ease-in-out ">
           {/* columna izquierda */}
-          <div className="w-full md:w-1/3">
+          <div className="w-full md:w-1/3 max-h-150 overflow-y-auto rounded-xl">
             <Accordion className="w-full shadow-sm">
-              {data.map((item: any) => (
+              {data.map((item: Movie) => (
                 <Accordion.Item key={item.id}>
                   <Accordion.Heading>
                     <Accordion.Trigger>
@@ -61,7 +68,7 @@ export default function HomeDashboard() {
                         className="mt-4 w-full bg-black text-white"
                         onClick={() => setSelectedMovie(String(item.id))}
                       >
-                        Watch movie
+                        Ver pelicula
                       </Button>
                     </Accordion.Body>
                   </Accordion.Panel>
@@ -69,6 +76,7 @@ export default function HomeDashboard() {
               ))}
             </Accordion>
           </div>
+
           {/* Columa de la derecha */}
           <div className="w-full md:w-2/3">
             {selectedMovie.length > 0 ? (
@@ -84,12 +92,12 @@ export default function HomeDashboard() {
             ) : (
               /* Ventana en espera */
               <div className="w-full aspect-video bg-gray-50 border border-gray-200 rounded-xl flex items-center justify-center text-gray-400 shadow-sm">
-                Select a movie to start
+                Selecciona una pelicula para comenzar
               </div>
             )}
           </div>
         </div>
-      )}
+      ) : null}
     </div>
   );
 }
