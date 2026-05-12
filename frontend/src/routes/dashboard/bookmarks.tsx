347d34f5b4eaa06b134cbefff1eace951b728bc9
Fabio Cuevas <fcuevas2505@gmail.com>
2026-05-12 12:32:12 -0600

    feat: crear componente MovieCarousel

--- frontend/src/routes/dashboard/bookmarks.tsx
+++ frontend/src/routes/dashboard/bookmarks.tsx
@@ -1,7 +1,40 @@
+import { MovieCarousel } from "@/components/ui/MovieCarousel";
+
 export default function bookmarkDashboardPage() {
   return (
     <>
-      <h1>Hola Bookmark Page</h1>
+      {/* prueba carrusel*/}
+      <div className="w-full mt-12">
+        <MovieCarousel
+          title="Tendencias de la semana"
+          movies={[
+            {
+              id: 1,
+              title: "The Batman",
+              poster:
+                "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+              overview:
+                "Cuando un asesino en serie sádico comienza a asesinar a figuras políticas clave en Gotham, Batman investiga la corrupción oculta.",
+            },
+            {
+              id: 2,
+              title: "John Wick 4",
+              poster:
+                "https://image.tmdb.org/t/p/w500/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg",
+              overview:
+                "John Wick descubre un camino para derrotar a la Alta Mesa. Pero antes debe enfrentarse a un nuevo enemigo.",
+            },
+            {
+              id: 3,
+              title: "Blade Runner 2049",
+              poster:
+                "https://image.tmdb.org/t/p/w500/gajva2L0rPYkEWjzgFlBXCAVBE5.jpg",
+              overview:
+                "Un nuevo blade runner descubre un secreto oculto que tiene el potencial de hundir a la sociedad en el caos.",
+            },
+          ]}
+        />
+      </div>
     </>
   );
 }
