c6c3a521eef1408fc1b4e3eeb32f30e2a6fee1c8
Juan Lozoya <juanlozoya733@gmail.com>
2026-05-12 16:06:04 -0600

    feat: add database configuration and models for user management

---
+++ backend/app/db/base.py
@@ -0,0 +1,2 @@
+from app.db.database import Base
+from app.db.models.user import User
--- backend/app/utils/keys.py
+++ backend/app/utils/keys.py
@@ -2,3 +2,4 @@ import os
 
 MovieDB_Key = os.getenv("MovieDB_KEY")
 Redis_URL = os.getenv("REDIS_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
